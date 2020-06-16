efe9543126f8969c0c7d3786f619566ea2488801
Marcin <marcingorecki1981@gmail.com>
2020-06-16 22:24:55 +0200

    questions update - second try

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -27,15 +28,12 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   int questionTracker = 0;
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
-  ];
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
 
   @override
@@ -50,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionTracker],
+                questions[questionTracker].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -76,7 +74,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(
                   () {
-                    if (answers[questionTracker] == true) {
+                    if (questions[questionTracker].questionAnswer == true) {
                       scoreKeeper.add(
                         Icon(
                           Icons.check,
@@ -93,8 +91,6 @@ class _QuizPageState extends State<QuizPage> {
                     }
 
                     if (questionTracker < questions.length - 1) {
-                      print(questions[questionTracker]);
-                      print(answers[questionTracker]);
                       questionTracker++;
                     }
                   },
@@ -118,7 +114,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(
                   () {
-                    if (answers[questionTracker] == false) {
+                    if (questions[questionTracker].questionAnswer == false) {
                       scoreKeeper.add(
                         Icon(
                           Icons.check,
@@ -135,8 +131,6 @@ class _QuizPageState extends State<QuizPage> {
                     }
 
                     if (questionTracker < questions.length - 1) {
-                      print(questions[questionTracker]);
-                      print(answers[questionTracker]);
                       questionTracker++;
                     }
                   },
